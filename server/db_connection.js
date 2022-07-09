32406eaa933134e68439e25a90729363e85f9754
unknown <quercia.matt@gmail.com>
2022-07-09 15:36:14 -0400

    test

--- server/db_connection.js
+++ server/db_connection.js
@@ -1,7 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-
+    user: 'postgres',
+    password: 'password',
+    host: 'localhost',
+    port: 5432,
+    database: 'twitter'
 });
 
 module.exports = pool;
